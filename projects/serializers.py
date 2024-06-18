5db3cf96921725a4907d4bf8274cc5d8331b4038
fabricioFalcao <fabriciohfd@gmail.com>
2024-06-18 09:33:12 -0300

    Requisito 3

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from projects.models import Profile
+from projects.models import Profile, Project
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
+        fields = "__all__"
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
         fields = "__all__"
